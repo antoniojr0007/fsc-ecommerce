3bfa6c7130be522c373d82c16875baaa5e0a78ec
Antonio Ferreira de OLiveira Junior <antonio-250291@hotmail.com>
2023-12-13 01:35:41 -0300

    feat: add first discount banner home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,22 @@
 "use client"
 
-import { useSession } from "next-auth/react";
+import Image from "next/image"
+
+
 
 export default function Home() {
 
-  const { data } = useSession();
 
   return (
-    <>
-      <div className="mx-auto w=[1920]">
-       <div>{data?.user?.name}</div>
-       <div>{data?.user?.email}</div> 
-      </div>
-    </>
+    <div className="p-5">
+      <Image 
+        src="/banner-home-01.png"
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="até 55% Desconto só esse mês"
+      />
+    </div>
   )
 }
